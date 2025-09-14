d3f876712cae1a844976a6e01871318edbf62ad6
Gino Reginato <gp.reginato@alumno.um.edu.ar>
2025-09-13 22:56:37 -0300

    Continuacion del test

--- tests/test_player.py
+++ tests/test_player.py
@@ -8,3 +8,16 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(p.get_name(), "Juan")
         self.assertEqual(p.get_color(), "X")
         self.assertEqual(p.get_checkers(), 15)
+
+    def test_remove_checker(self):
+        p = Player("Ana", "O")
+        p.remove_checker()
+        self.assertEqual(p.get_checkers(), 14)
+
+    def test_add_checker(self):
+        p = Player("Luis", "X")
+        p.add_checker()
+        self.assertEqual(p.get_checkers(), 16)
+
+if __name__ == "__main__":
+    unittest.main()
